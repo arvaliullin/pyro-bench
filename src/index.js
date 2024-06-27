238504612062ae2b4d75a8c69cf62e3ae7a3f463
Artur Valiullin <ar.valiullin@yandex.ru>
2024-06-27 21:38:18 +0300

    save

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import fs from 'fs';
+import path from 'path';
 import { performanceExperiments } from './performance/performance.js';
 
 import './wasm_exec.js';
@@ -8,22 +9,43 @@ const filename = `${cwd}/public/lib_go.out.wasm`;
 const source = fs.readFileSync(filename);
 const typedArray = new Uint8Array(source);
 
-
+const outputDir = path.join(cwd, 'out');
+if (!fs.existsSync(outputDir)) {
+    fs.mkdirSync(outputDir);
+}
 
 const runPerformanceExperiment = async (experimentId) => {
     const experiment = performanceExperiments[experimentId];
-    return experiment.calculatePerformance();
-    };
+    console.log(`Выполнение эксперимента: ${experiment.experimentName}`);
+    
+    const performanceValues = await experiment.calculatePerformance();
 
-const handleRecalculate = async () => {
-    const experimentId = "fibonacciRecursive";
-    runPerformanceExperiment(experimentId)
-        .then((performanceValues) => {
-            console.log(performanceValues);
-        })
-        .catch(() => {
-            console.error("Error");
-        });
+    // Вычисление значения Acceleration для каждого измерения
+    performanceValues.forEach(value => {
+        value.Acceleration = value.JavaScript / value.WebAssembly;
+    });
+
+    const csvData = convertToCSV(performanceValues);
+    fs.writeFileSync(path.join(outputDir, `${experimentId}.csv`), csvData);
+    
+    console.log(`Эксперимент ${experiment.experimentName} завершен.`);
+};
+
+const convertToCSV = (data) => {
+    const headers = Object.keys(data[0]).join(',');
+    const rows = data.map(row => Object.values(row).join(',')).join('\n');
+    return `${headers}\n${rows}`;
+};
+
+const runAllExperiments = async () => {
+    for (const experimentId in performanceExperiments) {
+        try {
+            await runPerformanceExperiment(experimentId);
+        } catch (error) {
+            console.error(`Ошибка выполнения эксперимента ${experimentId}: ${error.message}`);
+        }
+    }
+    console.log('Все эксперименты завершены.');
 };
 
 const runWasm = async () => {
@@ -31,8 +53,8 @@ const runWasm = async () => {
     const result = await WebAssembly.instantiate(typedArray, go.importObject);
     go.run(result.instance);
 
-    handleRecalculate().then(() => {
-        console.log("Success");
+    runAllExperiments().then(() => {
+        console.log("Выполнение всех экспериментов успешно завершено.");
     });
 };
 
