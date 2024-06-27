a26d0208dbaf165e91aa8a9bb427a93e88643836
Artur Valiullin <ar.valiullin@yandex.ru>
2024-06-27 21:43:32 +0300

    save

---
+++ scripts/build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+cp -rf $PWD/dist $PWD/out
+
+cp "$(go env GOROOT)/misc/wasm/wasm_exec.js" $PWD/src
+
+# Build Go WebAssembly
+export GOARCH=wasm
+export GOOS=js
+go build -o $PWD/public/lib_go.out.wasm $PWD/pkg/lib.go
+
+bun run start
